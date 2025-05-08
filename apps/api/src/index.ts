7c78b93c6c9f5bd0c201b59f411ec9a63155a942
Vitor Tognetti Baradelli <vitorbaradelli@gmail.com>
2025-05-08 16:30:53 -0300

    feat(auth): implement role-based permissions and user model

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,6 @@
-import { ability } from '@sass/auth'
+import { defineAbilityFor } from '@sass/auth'
+
+const ability = defineAbilityFor({ role: 'ADMIN' })
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
 const userCanDeleteUsers = ability.can('delete', 'User')
